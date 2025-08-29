5f1eade0c2d3c3d8ff54825d080308eca52a4f56
Kermit <k84101311@gmail.com>
2025-08-29 19:40:55 +0800

    init project

---
+++ src/main/java/com/gtelant/commerce_service/CommerceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gtelant.commerce_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtelant/commerce_service/CommerceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtelant.commerce_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
