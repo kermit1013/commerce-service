365b056c5303f354fe20a65663c611d697314ddb
Kermit <k84101311@gmail.com>
2025-08-31 09:53:49 +0800

    複合主鍵範例

--- src/main/java/com/gtelant/commerce_service/models/UserSegment.java
+++ src/main/java/com/gtelant/commerce_service/models/UserSegment.java
@@ -13,9 +13,10 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class UserSegment {
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private int id;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
@@ -31,3 +32,31 @@ public class UserSegment {
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
 }
+
+
+/* 複合主鍵
+@Entity
+@Table(name = "user_segments")
+@IdClass(UserSegment.class)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserSegment {
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "segment_id")
+    private Segment segment;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
+*/
